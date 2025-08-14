d325bccf00907b897d4a9c4bc3b4b469f09bc3bc
DiegoS0409 <diegodelvalle91@gmail.com>
2025-08-14 11:19:02 -0400

    Primer Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const botonAgregar = 
+document.querySelector('.add-btn');
+
+botonAgregar.addEventListener('click', function() {
+    botonAgregar.remove();
+});
+
+const botonesLike = document.querySelectorAll('.like-btn');
+
+botonesLike.forEach(function(boton) {
+    boton.addEventListener('click', function(event) {
+        
+        let textoActual = boton.innerText;
+        let partes = textoActual.split(' ');
+        let contadorActual = parseInt(partes[0]);
+        contadorActual++;
+        boton.innerText = contadorActual + ' Me gusta';
+
+        const cartaPadre = event.target.closest('.carta');
+        const tituloMascota = cartaPadre.querySelector('h2').innerText;
+        alert('Te gustó la definición de ' + tituloMascota);
+    });
+});
