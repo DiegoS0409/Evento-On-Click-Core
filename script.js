bcb4577dac36e3d40a628709aff59ea83097d2ed
DiegoS0409 <diegodelvalle91@gmail.com>
2025-08-14 11:30:52 -0400

    Segundo commit

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ botonAgregar.addEventListener('click', function() {
     botonAgregar.remove();
 });
 
-const botonesLike = document.querySelectorAll('.like-btn');
+const botonesLike = 
+document.querySelectorAll('.like-btn');
 
 botonesLike.forEach(function(boton) {
     boton.addEventListener('click', function(event) {
-        
+
         let textoActual = boton.innerText;
         let partes = textoActual.split(' ');
         let contadorActual = parseInt(partes[0]);
         contadorActual++;
         boton.innerText = contadorActual + ' Me gusta';
+
 
         const cartaPadre = event.target.closest('.carta');
         const tituloMascota = cartaPadre.querySelector('h2').innerText;
